8b708e82b7f12d4a1ce77d585b0d8b022115771f
jhoughtelin <josh@findsomehelp.com>
2015-03-17 15:19:02 -0600

    Added blank Rate & Amenity classes

---
+++ lib/Property/Amenity.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file Amenity.php
+ * @project VRPConnector
+ * @author Josh Houghtelin <josh@findsomehelp.com>
+ * @created 3/17/15 3:18 PM
+ */
+
+namespace Gueststream\Property;
+
+
+class Amenity {
+
+}
---
+++ lib/Property/Rate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file Rate.php
+ * @project VRPConnector
+ * @author Josh Houghtelin <josh@findsomehelp.com>
+ * @created 3/17/15 3:18 PM
+ */
+
+namespace Gueststream\Property;
+
+
+class Rate {
+
+}

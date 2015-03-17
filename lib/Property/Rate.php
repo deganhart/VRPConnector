3dfa81e5cd3e3a3a8641ce272f29c5cad17b5d08
jhoughtelin <josh@findsomehelp.com>
2015-03-17 15:29:11 -0600

    built out Rate class.

--- lib/Property/Rate.php
+++ lib/Property/Rate.php
@@ -10,5 +10,99 @@ namespace Gueststream\Property;
 
 
 class Rate {
+    protected $startDate;
+    protected $endDate;
+    protected $amount;
+    protected $chargeBasis;
+
+    public function __construct($data)
+    {
+        foreach($data as $key => $val) {
+            $setter = "set".ucfirst($key);
+            if(method_exists($this,$setter)) {
+                $this->$setter($val);
+            }
+        }
+    }
+
+    public function __get($name)
+    {
+        if(isset($this->$name)) {
+            return $this->$name;
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $setter = "set".ucfirst($name);
+        if(method_exists($this,$setter)) {
+            return $this->$setter($value);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param mixed $startDate
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param mixed $endDate
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param mixed $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChargeBasis()
+    {
+        return $this->chargeBasis;
+    }
+
+    /**
+     * @param mixed $chargeBasis
+     */
+    public function setChargeBasis($chargeBasis)
+    {
+        $this->chargeBasis = $chargeBasis;
+    }
+
 
 }

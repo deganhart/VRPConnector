f95cd817623bcb8752a24bce8b4c8c65ba209273
jhoughtelin <josh@findsomehelp.com>
2015-03-17 15:15:12 -0600

    Creating Unit class

---
+++ lib/Property/Unit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file Unit.php
+ * @project VRPConnector
+ * @author Josh Houghtelin <josh@findsomehelp.com>
+ * @created 3/17/15 3:14 PM
+ */
+
+namespace Gueststream\Property;
+
+
+class Unit {
+
+}

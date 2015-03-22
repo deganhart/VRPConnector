7ea9f557407f091a595eafa697b938165972aaba
Zechariah Walden <zech@zewadesign.com>
2015-03-22 16:52:31 -0500

    changes to css, result layout html

--- themes/mountainsunset/results.php
+++ themes/mountainsunset/results.php
@@ -1,13 +1,13 @@
 
 <!-- all the important responsive layout stuff -->
 <div id="vrp">
-    <div id="vrpresults" class="vpr-container-fluid">
+    <div id="vrpresults" class="vrp-container-fluid">
 
 
 
         <?php if ($data->count === 0) : ?>
 
-            <div class="vpr-row">
+            <div class="vrp-row">
                 <h2>No Results Found</h2>
                 <p>Please revise your search criteria.</p>
             </div>
@@ -15,69 +15,73 @@
         <?php else: ?>
 
         <!-- Total number of results found -->
-        <div class="row">
+        <div class="vrp-row">
 
-            <div class="vpr-col-md-12 vpr-filter-result-handles">
-                <a href="<?php echo site_url() . "/vrp/favorites/show";?>" class="vpr-btn vpr-stylized turquoise vpr-pull-left">
-                    <span>
+            <div class="vrp-col-md-3 vrp-filter-result-handles">
+                <a href="<?php echo site_url() . "/vrp/favorites/show";?>" class="vrp-btn vrp-stylized turquoise vrp-pull-left">
+                    <div class="vrp-icon">
                         <i class="fa fa-fw fa-2x fa-heart"></i>
-                    </span>
+                    </div>
                     View Favorites
                 </a>
-                <div class="vpr-pull-right">
+            </div>
+            <div class="vrp-col-md-9">
+                <div class="vrp-pull-right">
                     <?php vrp_resultsperpage(); ?>
                     <?php vrpsortlinks($data->results[0]); ?>
 
-                    <a href="#" id="vpr-list" class="btn">
+                    <a href="#" id="vrp-list" class="btn">
                         <i class="fa fa-fw fa-lg fa-th-list"></i>
 
                     </a>
-                    <a href="#" id="vpr-grid" class="btn">
+                    <a href="#" id="vrp-grid" class="btn">
                         <i class="fa fa-fw fa-lg fa-th"></i>
                     </a>
                 </div>
             </div>
         </div>
 
-        <div class="vpr-row">
-            <div class="vpr-col-xs-12"><hr /></div>
+        <div class="vrp-row">
+            <div class="vrp-col-xs-12"><hr /></div>
         </div>
 
-        <div class="vpr-row">
+        <div class="vrp-row">
         <?php foreach($data->results as $index => $a_unit) : ?>
 
 
-            <div class="vpr-col-md-4 vpr-col-xs-6 vpr-col-sm-12 vpr-list-item-wrap vpr-grid">
-                <div class="vpr-item">
+            <div class="vrp-col-md-4 vrp-col-xs-12 vrp-col-sm-6 vrp-list-item-wrap vrp-grid">
+                <div class="vrp-item">
 
-                    <div class="vpr-thumbnail text-center" style="background-image:url(<?=esc_url($a_unit->Thumb); ?>);">
+                    <div class="vrp-thumbnail text-center" style="background-image:url(<?=esc_url($a_unit->Thumb); ?>);">
 
-                        <div class="vpr-actions text-center">
-                            <a href="<?php echo site_url() . "/vrp/favorites/show";?>" data-unit="<?php echo $a_unit->id; ?>" class="vrp-favorite-button vpr-btn purple">
+                        <div class="vrp-actions">
+                            <a href="<?php echo site_url() . "/vrp/favorites/show";?>" data-unit="<?php echo $a_unit->id; ?>" class="vrp-favorite-button vrp-btn purple text-center">
                                 <i class="fa fa-fw fa-lg fa-heart"></i>
                                 Add to favorites
                             </a>
-                            <br /><br />
-                            <a href="#" data-unit="<?php echo $a_unit->id; ?>" class="vpr-btn orange">
+
+                            <a href="#" data-unit="<?php echo $a_unit->id; ?>" class="vrp-btn orange">
                                 <i class="fa fa-fw fa-lg fa-calendar"></i>
                                 Reserve now
                             </a>
                         </div>
-<!--                        <img src="http://placehold.it/285x200/ddd/ddd" class="vpr-img-thumbnail vpr-img-responsive vpr-center-block" width="97%" alt="" /> -->
+<!--                        <img src="http://placehold.it/285x200/ddd/ddd" class="vrp-img-thumbnail vrp-img-responsive vrp-center-block" width="97%" alt="" /> -->
                     </div>
 
-                    <div class="vpr-caption">
-                        <div class="vpr-row">
-                            <div class="vpr-col-md-8">
+                    <div class="vrp-caption">
+                        <div class="vrp-row">
+                            <div class="vrp-col-xs-8 vrp-col-sm-7">
                                 <h4><?=esc_html($a_unit->Name); ?></h4>
                             </div>
-                            <div class="vpr-col-md-4">
-                                <span class="vpr-epink">
-                                    <strong><?=esc_html($a_unit->Bedrooms); ?> Beds</strong>
-                                </span>
-                                <span class="vpr-kiwi">
-                                    <strong><?=esc_html($a_unit->Bathrooms); ?> Baths</strong>
-                                </span>
+                            <div class="vrp-col-xs-4 vrp-col-sm-5">
+                                <div class="vpr-meta-wrapper pull-right">
+                                    <span class="vrp-epink">
+                                        <strong><?=esc_html($a_unit->Bedrooms); ?> Beds</strong>
+                                    </span>
+                                    <span class="vrp-kiwi">
+                                        <strong><?=esc_html($a_unit->Bathrooms); ?> Baths</strong>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -87,8 +91,8 @@
         <?php endforeach; ?>
         </div>
         <?php endif ?>
-        <div class="vpr-row">
-            <div class="vpr-col-md-12">
+        <div class="vrp-row">
+            <div class="vrp-col-md-12">
                 <?=vrp_pagination($data->totalpages, $data->page); ?>
             </div>
         </div>
